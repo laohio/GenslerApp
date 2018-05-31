dbdac2e9bb9328d12fc63c805787e214f8cc2f88
Dennis <dennis.kim@tufts.edu>
2018-05-31 14:00:34 -0400

    changed specification of port for localhost

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
+const port = process.env.PORT || 5000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 5000");
+});
 
 // public is directory for all static files
 app.use(express.static(__dirname + '/public'));
@@ -12,6 +15,3 @@ app.use(express.static("."));
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname + '/index.html'));
 })
-
-// listen on the port specified ('set') earlier in the file
-app.listen(app.get('port'));
